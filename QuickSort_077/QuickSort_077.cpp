e1ee6a9fbac0fe4b011179fbf7ae957e53d78463
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-03-29 14:32:34 +0700

    mengurutkan sub array

--- QuickSort_077/QuickSort_077.cpp
+++ QuickSort_077/QuickSort_077.cpp
@@ -80,8 +80,9 @@ void q_short(int low, int high)
 	if (low < j) { //step 11
 		// swap the pivot element with the element at index j
 		swap(low, j);
-
-
 	}
+
+	//recursive call to sort the left sub array
+	q_short(low, j - 1); //step 12
 
 }
